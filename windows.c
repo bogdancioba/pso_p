0d5875f6e8bdfc4dce0c602c140a8c00b96ee802
bogdancioba <cioba.bogdan@gmail.com>
2022-10-31 15:51:52 +0200

    commit

--- windows.c
+++ windows.c
@@ -1,8 +1,37 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <pwd.h>
 
-int main()
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[1;31m"
+#define KGRN "\x1B[1;32m"
+#define KYEL "\x1B[1;33m"
+#define KBLU "\x1B[1;34m"
+#define KMAG "\x1B[1;35m"
+#define KCYN "\x1B[1;36m"
+#define KWHT "\x1B[1;37m"
+
+int main(int argc, char **argv)
 {
-  int age;
-  printf("Enter age\n");
+#ifdef _WIN32 // Includes both 32 bit and 64 bit
+  if (argc < 2)
+  {
+    printf("%sGive a filename%s\n", KYEL, KNRM);
+    return 1;
+  }
 
+  if (access(argv[1], F_OK) == -1)
+  {
+    printf("%sError:%s file does not exist\n", KRED, KNRM);
+    return 2;
+  }
+
+  struct stat st;
+  stat(argv[1], &st);
+
+  struct passwd *pw = getpwuid(st.st_uid);
+  printf("%sFile %s has owner %s%s\n", KGRN, argv[1], pw->pw_name, KNRM);
+#endif
+  return 0;
 }
